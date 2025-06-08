1772413eec95a503fbed3abf72b6f4d93af96062
idasoderlund <soderlundida7711@gmail.com>
2025-06-08 03:13:16 +0200

    trying to solve error

--- src/main.jsx
+++ src/main.jsx
@@ -18,16 +18,3 @@ root.render(
     </StrictMode>
   </RegistrationProvider>
 );
-
-/*
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RegistrationProvider>
-    <App />
-  </RegistrationProvider>
-);*/
