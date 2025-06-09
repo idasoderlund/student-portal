3ef4c9425d4bfcd5f41a33ccf4bab138467c835d
idasoderlund <soderlundida7711@gmail.com>
2025-06-09 22:49:16 +0200

    added css props to course.name and course.detail

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -7,11 +7,23 @@ function CourseDetails() {
   if (!course) {
     return <p>Course is not found.</p>;
   }
+  const nameStyle = {
+    fontFamily: "'Poiret One', sans-serif",
+    fontSize: "40px",
+    color: "#EEEFE0",
+    marginBottom: "20px",
+  };
 
+  const detailStyle = {
+    fontFamily: "'Poiret One', sans-serif",
+    fontSize: "30px",
+    lineHeight: "1.5",
+    color: "#EEEFE0",
+  };
   return (
     <div>
-      <h2>{course.name}</h2>
-      <p>{course.details}</p>
+      <h2 style={nameStyle}>{course.name}</h2>
+      <p style={detailStyle}>{course.details}</p>
     </div>
   );
 }
