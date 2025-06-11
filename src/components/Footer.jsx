4b76933a5551b92a01d8e48a7ac95022d3f2094d
idasoderlund <soderlundida7711@gmail.com>
2025-06-11 22:31:43 +0200

    added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div
+      className="footer-container"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        height: "60px",
+        width: "100%",
+        color: "#6f826a",
+        backgroundColor: "#D1D8BE",
+        marginTop: "195px",
+        paddingTop: "25px",
+      }}
+    >
+      <p style={{ gridColumn: "1", marginLeft: "120px" }}>
+        {" "}
+        <b>Email:</b> student.portal@fake.sun
+      </p>
+      <p style={{ gridColumn: "2", marginLeft: "140px" }}>
+        <b>Phone:</b> 021-000 00
+      </p>
+      <p style={{ gridColumn: "3", marginLeft: "120px" }}>
+        {" "}
+        <b>Visit us:</b> Fakeroad 7B
+      </p>
+    </div>
+  );
+}
+export default Footer;
