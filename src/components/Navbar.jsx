d00283bf23eac8a4ae72b48d3252084b766d5273
idasoderlund <soderlundida7711@gmail.com>
2025-06-11 21:46:30 +0200

    added margin to links in navbar screens >769px

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,15 @@ function NavigationBar() {
   };
 
   return (
-    <Navbar className="navbar-container" style={{ backgroundColor: "#EEEFE0" }}>
+    <Navbar
+      className="navbar-container"
+      style={{
+        backgroundColor: "#EEEFE0",
+        /*display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",*/
+      }}
+    >
       <Container>
         <Navbar.Brand
           style={{
@@ -26,84 +34,92 @@ function NavigationBar() {
         </Navbar.Brand>
 
         <Navbar.Collapse className="desktop-nav">
-          <Nav
-            className="nav-links-container"
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              width: "100%",
-            }}
-          >
-            <Nav.Link
-              to="/"
-              as={Link}
-              className="nav-link1"
+          <div className="nav-large-screens">
+            <Nav
+              className="nav-links-container"
               style={{
-                fontFamily: "'Poiret One', sans-serif",
-                fontSize: "20px",
-                backgroundColor: "#D1D8BE",
-                margin: "4px 8px",
-                borderRadius: "8px",
                 display: "flex",
                 justifyContent: "space-around",
-                alignItems: "center",
-                boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
+                width: "100%",
               }}
             >
-              Home
-            </Nav.Link>
+              <Nav.Link
+                to="/"
+                as={Link}
+                className="nav-link1"
+                style={{
+                  fontFamily: "'Poiret One', sans-serif",
+                  fontSize: "20px",
+                  backgroundColor: "#D1D8BE",
+                  margin: "4px 8px",
+                  marginLeft: "80px",
+                  marginRight: "60px",
+                  borderRadius: "8px",
+                  boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
+                }}
+              >
+                Home
+              </Nav.Link>
 
-            <Nav.Link
-              to="/courses"
-              as={Link}
-              className="nav-link2"
-              style={{
-                fontFamily: "'Poiret One', sans-serif",
-                fontSize: "20px",
-                backgroundColor: "#D1D8BE",
-                margin: "4px",
-                borderRadius: "8px",
-                padding: "5px 10px",
-                boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
-              }}
-            >
-              Courses
-            </Nav.Link>
+              <Nav.Link
+                to="/courses"
+                as={Link}
+                className="nav-link2"
+                style={{
+                  fontFamily: "'Poiret One', sans-serif",
+                  fontSize: "20px",
+                  backgroundColor: "#D1D8BE",
+                  margin: "4px",
+                  marginLeft: "60px",
+                  marginRight: "60px",
+                  borderRadius: "8px",
+                  padding: "5px 10px",
+                  boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
+                }}
+              >
+                Courses
+              </Nav.Link>
 
-            <Nav.Link
-              as={Link}
-              className="nav-link3"
-              to="/news"
-              style={{
-                fontSize: "20px",
-                fontFamily: "'Poiret One', sans-serif",
-                backgroundColor: "#D1D8BE",
-                margin: "4px",
-                borderRadius: "8px",
-                padding: "5px 10px",
-                boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
-              }}
-            >
-              News
-            </Nav.Link>
+              <Nav.Link
+                as={Link}
+                className="nav-link3"
+                to="/news"
+                style={{
+                  fontSize: "20px",
+                  fontFamily: "'Poiret One', sans-serif",
+                  backgroundColor: "#D1D8BE",
+                  margin: "4px",
+                  marginLeft: "60px",
+                  marginRight: "80px",
+                  borderRadius: "8px",
+                  padding: "5px 10px",
+                  boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
+                }}
+              >
+                News
+              </Nav.Link>
 
-            <Nav.Link
-              as={Link}
-              to="/register"
-              className="nav-link4"
-              style={{
-                fontSize: "20px",
-                fontFamily: "'Poiret One', sans-serif",
-                backgroundColor: "#D1D8BE",
-                margin: "4px",
-                borderRadius: "8px",
-                padding: "5px 10px",
-                boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
-              }}
-            >
-              Registration
-            </Nav.Link>
-          </Nav>
+              <Nav.Link
+                as={Link}
+                to="/register"
+                className="nav-link4"
+                style={{
+                  fontSize: "20px",
+                  fontFamily: "'Poiret One', sans-serif",
+                  backgroundColor: "#D1D8BE",
+                  margin: "4px",
+                  marginLeft: "60px",
+                  marginRight: "0",
+                  borderRadius: "8px",
+                  padding: "5px 10px",
+                  boxShadow: "2px 4px 8px rgba(0,0,0,0.7)",
+                }}
+              >
+                Registration
+              </Nav.Link>
+            </Nav>
+          </div>
+
           <div
             className="responsiveMenu"
             onClick={toggleMobileMenu}
@@ -115,7 +131,6 @@ function NavigationBar() {
             <div className="bar"></div>
             <div className="bar"></div>
           </div>
-
           {isMobileMenuOpen && (
             <div className="mobile-menu">
               <Link
